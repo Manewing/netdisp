bad56b2f2ad547f6cb38a2e9a1b1bb1ea0875bd9
Florian Walbroel <florian@hswalbroel.de>
2021-01-02 19:43:59 +0100

    add async udp receiver

--- components/network/include/network/UdpReceiver.hpp
+++ components/network/include/network/UdpReceiver.hpp
@@ -1,13 +1,14 @@
 #ifndef NETWORK_UDP_RECEIVER_HPP
 #define NETWORK_UDP_RECEIVER_HPP
 
+#include <network/AsyncReceiver.hpp>
 #include <network/Receiver.hpp>
 
 namespace network {
 
 class UdpReceiver : public Receiver {
 public:
-  UdpReceiver(int Port, bool IPv6 = false);
+  UdpReceiver(int Port, bool Ipv6 = false);
   virtual ~UdpReceiver();
 
   bool isReady() const;
@@ -20,6 +21,18 @@ private:
   int Socket = -1;
 };
 
-}
+class AsyncUdpReceiver : protected AsyncReceiver {
+public:
+  AsyncUdpReceiver(int Port, unsigned MaxMsgLen, bool Ipv6 = false);
+
+  using AsyncReceiver::onRecv;
+
+  bool isReady() const override;
+
+private:
+  UdpReceiver UdpRecv;
+};
+
+} // namespace network
 
 #endif // #ifndef NETWORT_UDP_RECEIVER_HPP
--- components/network/src/UdpReceiver.cpp
+++ components/network/src/UdpReceiver.cpp
@@ -8,14 +8,14 @@
 
 namespace network {
 
-UdpReceiver::UdpReceiver(int Port, bool IPv6) {
-  Socket = socket(IPv6 ? AF_INET6 : AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+UdpReceiver::UdpReceiver(int Port, bool Ipv6) {
+  Socket = socket(Ipv6 ? AF_INET6 : AF_INET, SOCK_DGRAM, IPPROTO_UDP);
   if (Socket < 0) {
     return;
   }
 
   struct sockaddr_in6 Addr;
-  if (IPv6) {
+  if (Ipv6) {
     Addr.sin6_family = AF_INET6;
     Addr.sin6_addr = in6addr_any;
     Addr.sin6_port = htons(Port);
@@ -29,15 +29,13 @@ UdpReceiver::UdpReceiver(int Port, bool IPv6) {
   int Enable = 1;
   int Ret = setsockopt(Socket, SOL_SOCKET, SO_REUSEADDR, &Enable, sizeof(int));
   if (Ret < 0) {
-    close(Socket);
-    Socket = -1;
+    close();
     return;
   }
 
   Ret = bind(Socket, reinterpret_cast<struct sockaddr *>(&Addr), sizeof(Addr));
-  if ( Ret < 0) {
-    close(Socket);
-    Socket = -1;
+  if (Ret < 0) {
+    close();
     return;
   }
 }
@@ -52,11 +50,18 @@ int UdpReceiver::recv(void *Dest, std::size_t Len) const {
 
 void UdpReceiver::close() {
   if (isReady()) {
-    close(Socket);
+    ::close(Socket);
   }
+  Socket = -1;
 }
 
+AsyncUdpReceiver::AsyncUdpReceiver(int Port, unsigned MaxMsgLen, bool Ipv6)
+    : AsyncReceiver(UdpRecv, MaxMsgLen), UdpRecv(Port, Ipv6) {
+  start();
+}
 
+bool AsyncUdpReceiver::isReady() const {
+  return UdpRecv.isReady() && AsyncReceiver::isReady();
 }
 
 } // namespace network
