850489be8bbe531424288bcb0edd023c15750824
Florian Walbroel <florian@hswalbroel.de>
2021-01-02 16:46:33 +0100

    add view controller

---
+++ main/include/netdisp/View.hpp
@@ -0,0 +1,74 @@
+#ifndef NETDISP_VIEW_HPP
+#define NETDISP_VIEW_HPP
+
+#include <memory>
+#include <vector>
+
+#include <netdisp/Display.hpp>
+
+namespace netdisp {
+
+class View {
+public:
+  virtual void show(DisplayController &DC) = 0;
+};
+
+class TextViewBase : public View {
+public:
+  TextViewBase(std::string Text);
+
+protected:
+  std::string Text;
+};
+
+class RawTextView : public TextViewBase {
+public:
+  RawTextView(std::string Text);
+
+  void show(DisplayController &DC) override;
+};
+
+class TextView : public TextViewBase {
+public:
+  TextView(std::string Text);
+
+  void show(DisplayController &DC) override;
+
+private:
+  void write(DisplayController &DC, std::size_t Start, std::size_t End);
+
+private:
+  unsigned Column = 0;
+  unsigned Line = 0;
+  bool WriteCentered = false;
+};
+
+class ViewController {
+public:
+  // use uint8_t instead of unsigned
+  ViewController(std::shared_ptr<View> DefaultView, unsigned MaxViews);
+
+  unsigned getMaxViews() const;
+
+  void show(DisplayController &DC);
+
+  bool setView(unsigned Idx, std::shared_ptr<View> V);
+  // delete view
+
+  bool selectCurrentViewIdx(unsigned Idx);
+  unsigned getCurrentViewIdx() const;
+
+  bool showView(unsigned Idx);
+  unsigned getShownViewIdx() const;
+
+private:
+  std::shared_ptr<View> DefaultView;
+  std::vector<std::shared_ptr<View>> Views;
+
+  unsigned CurrentViewIdx = 0;
+  unsigned ShownViewIdx = 0;
+};
+
+} // namespace netdisp
+
+#endif // #ifndef NETDISP_VIEW_HPP
---
+++ main/src/View.cpp
@@ -0,0 +1,122 @@
+#include <algorithm>
+#include <cassert>
+#include <netdisp/View.hpp>
+
+namespace netdisp {
+
+TextViewBase::TextViewBase(std::string Txt) : Text(std::move(Txt)) {}
+
+RawTextView::RawTextView(std::string Txt) : TextViewBase(std::move(Txt)) {}
+
+void RawTextView::show(DisplayController &DC) {
+  DC.clear();
+  DC.setFontStyle(DisplayController::FontStyle::NORMAL);
+  DC.write(Text.c_str(), /*Line=*/0, /*Column=*/0, /*Wrap=*/true);
+}
+
+TextView::TextView(std::string Txt) : TextViewBase(std::move(Txt)) {}
+
+void TextView::show(DisplayController &DC) {
+  static const std::string FormatChars = "*_~\n";
+
+  // Clear screen and set normal style
+  DC.clear();
+  DC.setFontStyle(DisplayController::FontStyle::NORMAL);
+
+  std::size_t Last = 0;
+  Line = 0;
+  Column = 0;
+  WriteCentered = false;
+
+  for (std::size_t Idx = 0; Idx < Text.size(); Idx++) {
+    if (FormatChars.find(Text[Idx]) == std::string::npos) {
+      continue;
+    }
+
+    // Actually output the text
+    write(DC, Last, Idx);
+    Last = Idx + 1;
+
+    switch (Text[Idx]) {
+    case '*':
+      DC.toggleFontStyle(DisplayController::FontStyle::BOLD);
+      break;
+    case '_':
+      DC.toggleFontStyle(DisplayController::FontStyle::ITALIC);
+      break;
+    case '\n':
+      Column = 0;
+      Line++;
+      break;
+    case '~':
+      WriteCentered = true;
+      break;
+    default:
+      break;
+    }
+  }
+
+  write(DC, Last, Text.size());
+}
+
+void TextView::write(DisplayController &DC, std::size_t Start,
+                     std::size_t End) {
+  int Count = End - Start;
+  if (Count <= 0) {
+    return;
+  }
+
+  auto TextSection = Text.substr(Start, Count);
+  if (WriteCentered) {
+    Column = (DC.getColumns() - TextSection.size()) / 2;
+  }
+  DC.write(TextSection.c_str(), Line, Column, /*Wrap=*/false);
+
+  Column += Count;
+  WriteCentered = false;
+}
+
+ViewController::ViewController(std::shared_ptr<View> DefView, unsigned MaxViews)
+    : DefaultView(std::move(DefView)), Views() {
+  Views.resize(MaxViews);
+}
+
+unsigned ViewController::getMaxViews() const { return Views.size(); }
+
+void ViewController::show(DisplayController &DC) {
+  if (!Views.at(ShownViewIdx)) {
+    DefaultView->show(DC);
+    return;
+  }
+  Views.at(ShownViewIdx)->show(DC);
+}
+
+bool ViewController::setView(unsigned Idx, std::shared_ptr<View> V) {
+  if (Idx > getMaxViews()) {
+    return false;
+  }
+  Views.at(Idx) = V;
+  return true;
+}
+
+bool ViewController::selectCurrentViewIdx(unsigned Idx) {
+  if (Idx > getMaxViews()) {
+    return false;
+  }
+  CurrentViewIdx = Idx;
+  return true;
+}
+
+unsigned ViewController::getCurrentViewIdx() const { return CurrentViewIdx; }
+
+bool ViewController::showView(unsigned Idx) {
+  if (Idx > getMaxViews()) {
+    return false;
+  }
+  ShownViewIdx = Idx;
+  return true;
+}
+
+unsigned ViewController::getShownViewIdx() const { return ShownViewIdx; }
+
+} // namespace netdisp
